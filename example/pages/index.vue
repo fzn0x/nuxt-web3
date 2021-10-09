257791af771e0d1c9bac419d38fbee18307b71db
fncolon <fncolon@pm.me>
2021-10-09 12:16:00 +0700

    ref: tweaks

--- example/pages/index.vue
+++ example/pages/index.vue
@@ -23,23 +23,12 @@ export default {
   methods: {
     async contractConnect() {
       if (this.isEthereumSupported()) {
-        // Ready to use Web3.js!
-        // Example Usage :
-        // let data = await this.getProduct(100);
-        // console.log(data);
+        // do something..
       }
     },
-    // async getProduct(upc) {
-    //   let contract = new window.web3.eth.Contract(abi, this.contractAddress);
-    //   let message = await contract.methods.getProduct(upc).call();
-    //   return message;
-    // },
     async isEthereumSupported() {
       if (window.ethereum) {
         window.web3 = new this.$Web3(window.ethereum);
-        // Using provider setting from nuxt config :
-        // window.web3 = new this.$web3;
-
         try {
           // Request account access
           await window.ethereum.enable();
